b3f94f24d00099aab10f5d8d4bcfad8f96569dd3
Jared Hensley <jahensle85@me.com>
2015-10-26 21:35:51 -0500

    first and last passed

--- src/underscore_tests.js
+++ src/underscore_tests.js
@@ -1,8 +1,8 @@
 /*jshint eqnull:true, expr:true*/
 
-var _ = { };
+var _ = {};
 
-(function() {
+(function () {
 
   /**
    * COLLECTIONS
@@ -15,70 +15,81 @@ var _ = { };
 
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
-  _.first = function(array, n) {
+  _.first = function (array, n) {
+    var newarry = [];
+    if (!n) {
+      return array[0];
+    }
+    if (n > array.length) {
+      n = array.length;
+    }
+    for (var i = 0; i < n; i++) {
+      newarry.push(array[i]);
+    }
+    return newarry;
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
-  _.last = function(array, n) {
+  _.last = function (array, n) {
+    var newArr = [];
+    if (!n) {
+      return array[array.length - 1];
+    }
+    if (n > array.length) {
+      n = array.length;
+    }
+    for (var i = array.length - 1; i >= array.length - n; i--) {
+      newArr.unshift(array[i]);
+    }
+    return newArr;
+
   };
 
   // Call iterator(value, key, collection) for each element of collection.
   // Accepts both arrays and objects.
-  _.each = function(collection, iterator) {
-  };
+  _.each = function (collection, iterator) {};
 
   // Returns the index at which value can be found in the array, or -1 if value
   // is not present in the array.
-  _.indexOf = function(array, target){
-  };
+  _.indexOf = function (array, target) {};
 
   // Return all elements of an array that pass a truth test.
-  _.filter = function(collection, iterator) {
-  };
+  _.filter = function (collection, iterator) {};
 
   // Return all elements of an array that don't pass a truth test.
-  _.reject = function(collection, iterator) {
-  };
+  _.reject = function (collection, iterator) {};
 
   // Produce a duplicate-free version of the array.
-  _.uniq = function(array) {
-  };
+  _.uniq = function (array) {};
 
 
   // Return the results of applying an iterator to each element.
-  _.map = function(array, iterator) {
-  };
+  _.map = function (array, iterator) {};
 
   // Takes an array of objects and returns and array of the values of
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
-  _.pluck = function(array, propertyName) {
-  };
+  _.pluck = function (array, propertyName) {};
 
   // Calls the method named by methodName on each value in the list.
-  _.invoke = function(list, methodName, args) {
-  };
+  _.invoke = function (list, methodName, args) {};
 
   // Reduces an array or object to a single value by repetitively calling
   // iterator(previousValue, item) for each item. previousValue should be
   // the return value of the previous iterator call.
-  _.reduce = function(collection, iterator, initialValue) {
-  };
+  _.reduce = function (collection, iterator, initialValue) {};
 
   // Determine if the array or object contains a given value (using `===`).
-  _.contains = function(collection, target) {
-  };
+  _.contains = function (collection, target) {};
 
 
   // Determine whether all of the elements match a truth test.
-  _.every = function(collection, iterator) {
-  };
+  _.every = function (collection, iterator) {};
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
-  _.some = function(collection, iterator) {
-  };
+  _.some = function (collection, iterator) {};
 
 
   /**
@@ -90,13 +101,11 @@ var _ = { };
 
   // Extend a given object with all the properties of the passed in
   // object(s).
-  _.extend = function(obj) {
-  };
+  _.extend = function (obj) {};
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
-  _.defaults = function(obj) {
-  };
+  _.defaults = function (obj) {};
 
 
   /**
@@ -106,8 +115,7 @@ var _ = { };
 
   // Return a function that can be called at most one time. Subsequent calls
   // should return the previously returned value.
-  _.once = function(func) {
-  };
+  _.once = function (func) {};
 
   // Memoize an expensive function by storing its results. You may assume
   // that the function takes only one argument and that it is a primitive.
@@ -115,8 +123,7 @@ var _ = { };
   // Memoize should return a function that when called, will check if it has
   // already computed the result for the given argument and return that value
   // instead if possible.
-  _.memoize = function(func) {
-  };
+  _.memoize = function (func) {};
 
   // Delays a function for the given number of milliseconds, and then calls
   // it with the arguments supplied.
@@ -124,43 +131,36 @@ var _ = { };
   // The arguments for the original function are passed after the wait
   // parameter. For example _.delay(someFunction, 500, 'a', 'b') will
   // call someFunction('a', 'b') after 500ms
-  _.delay = function(func, wait) {
-  };
+  _.delay = function (func, wait) {};
 
 
 
   // Shuffle an array.
-  _.shuffle = function(array) {
-  };
+  _.shuffle = function (array) {};
 
   // Sort the object's values by a criterion produced by an iterator.
   // If iterator is a string, sort objects by that property with the name
   // of that string. For example, _.sortBy(people, 'name') should sort
   // an array of people by their name.
-  _.sortBy = function(collection, iterator) {
-  };
+  _.sortBy = function (collection, iterator) {};
 
   // Zip together two or more arrays with elements of the same index
   // going together.
   //
   // Example:
   // _.zip(['a','b','c','d'], [1,2,3]) returns [['a',1], ['b',2], ['c',3], ['d',undefined]]
-  _.zip = function() {
-  };
+  _.zip = function () {};
 
   // Takes a multidimensional array and converts it to a one-dimensional array.
   // The new array should contain all elements of the multidimensional array.
-  _.flatten = function(nestedArray, result) {
-  };
+  _.flatten = function (nestedArray, result) {};
 
   // Takes an arbitrary number of arrays and produces an array that contains
   // every item shared between all the passed-in arrays.
-  _.intersection = function() {
-  };
+  _.intersection = function () {};
 
   // Take the difference between one array and a number of other arrays.
   // Only the elements present in just the first array will remain.
-  _.difference = function(array) {
-  };
+  _.difference = function (array) {};
 
 }).call(this);
